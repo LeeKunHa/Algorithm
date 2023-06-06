fe33950c7aa130ed8135f2150c3b622f62101788
LeeKunHa <88371786+LeeKunHa@users.noreply.github.com>
2023-06-07 08:09:26 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,9 +2,9 @@ num = int(input())
 time_ = list(map(int, input().split()))
 
 time_ = sorted(time_)
+local_wait = 0
 global_wait = 0
-total_wait = 0
 for i in time_:
-  global_wait = global_wait+i
-  total_wait = total_wait+global_wait
-print(total_wait)
+  local_wait = local_wait+i
+  global_wait = global_wait+local_wait
+print(global_wait)
