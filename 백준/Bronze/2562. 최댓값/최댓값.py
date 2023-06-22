35c3fb62fcc8c8b612828eb405f9fda7a238e953
LeeKunHa <88371786+LeeKunHa@users.noreply.github.com>
2023-06-23 00:08:05 +0900

    [Bronze III] Title: 최댓값, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,13 @@
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+num_5 = int(input())
+num_6 = int(input())
+num_7 = int(input())
+num_8 = int(input())
+num_9 = int(input())
+
+num_list = [num_1,num_2,num_3,num_4,num_5,num_6,num_7,num_8,num_9]
+print(max(num_list))
+print(num_list.index(max(num_list))+1)
