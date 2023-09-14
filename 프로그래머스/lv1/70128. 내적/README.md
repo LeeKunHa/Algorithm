4995987db31260787327087d2bd856264b5623d1
LeeKunHa <kunha98@gmail.com>
2023-09-14 23:16:04 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/README.md
+++ 프로그래머스/lv1/70128. 내적/README.md
@@ -1,10 +1,10 @@
 # [level 1] 내적 - 70128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70128) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70128?language=python3) 
 
 ### 성능 요약
 
-메모리: 28.2 MB, 시간: 0.14 ms
+메모리: 10.1 MB, 시간: 0.10 ms
 
 ### 구분
 
--- 프로그래머스/lv1/70128. 내적/내적.py
+++ 프로그래머스/lv1/70128. 내적/내적.py
@@ -1,4 +1,11 @@
-import numpy as np
+#import numpy as np
+#def solution(a, b):
+#    answer = int(np.dot(a,b))
+#    return answer
+
 def solution(a, b):
-    answer = int(np.dot(a,b))
+    answer = 0
+    for i in range(len(a)):
+        single = a[i]*b[i]
+        answer = answer+single
     return answer
