6d2e5e62e5d9cba90bdfd4c472387847114c6192
LeeKunHa <kunha98@gmail.com>
2023-09-14 23:57:19 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,9 +1,5 @@
 def solution(s):
     answer = False
     if (len(s)==4) or (len(s)==6):
-        try:
-            sum(list(map(int, s)))
-            answer = True
-        except:
-            pass
+        answer = s.isdigit()
     return answer
