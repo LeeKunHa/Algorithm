8578e03ddd68ee2a8136c61d4606e0fa04e80514
LeeKunHa <kunha98@gmail.com>
2023-09-19 17:50:19 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(progresses, speeds):
+    answer = []
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+    while progresses!=deque([]):
+        progresses = deque([x+j for x,j in zip(progresses,speeds)])
+        if progresses[0] >= 100:
+            count = 0
+            while progresses!=deque([]) and progresses[0] >= 100:
+                progresses.popleft()
+                speeds.popleft()
+                count = count+1
+            answer.append(count)
+    return answer
