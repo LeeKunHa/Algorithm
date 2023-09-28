fd81f645f2d2193ecfa1c81ff6c6026c569fa500
LeeKunHa <kunha98@gmail.com>
2023-09-29 05:29:56 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -2,8 +2,6 @@ def solution(n):
     count = [x for x in range(1,n+1) if '3' in str(x) or x%3==0]
     while len(count)!=0:
         n = n+len(count)
-        print(n)
         count = [x for x in range(n-len(count)+1,n+1) if '3' in str(x) or x%3==0]
-        print(count)
     answer = n
     return answer
