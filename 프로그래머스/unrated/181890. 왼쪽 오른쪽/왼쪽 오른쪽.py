3f55534f988c008b87eaac2a8969d962a3c1d634
LeeKunHa <kunha98@gmail.com>
2023-10-01 02:39:34 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,15 +1,7 @@
 def solution(str_list):
-    l_index = ''.join(str_list).find('l')
-    r_index = ''.join(str_list).find('r')
-    if l_index == r_index == -1:
-        answer = []
-    elif l_index == -1:
-        answer = str_list[r_index+1:]
-    elif r_index == -1:
-        answer = str_list[:l_index]
-        
-    elif l_index<r_index:
-        answer = str_list[:l_index]
-    else:
-        answer = str_list[r_index+1:]
-    return answer
+    for i in range(len(str_list)):
+        if str_list[i]=='l':
+            return str_list[:i]
+        elif str_list[i]=='r':
+            return str_list[i+1:]
+    return []
