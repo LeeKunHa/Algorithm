789a83d5ada27ab221222131b2e84873683544af
LeeKunHa <kunha98@gmail.com>
2023-10-05 02:31:37 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -18,4 +18,31 @@ def solution(genres, plays):
             answer.append(k)
         before = i
     return answer
+
+
+def solution(genres, plays):
+    answer = []
+    dic = {x:0 for x in genres}
+    for i,j in zip(genres,plays):
+        dic[i] = dic[i]+j
+    alb = [[i,y,x] for i,x,y in zip(range(len(plays)),genres,plays)]
+    alb = sorted([x for x in alb], key=lambda x:(dic[x[2]],x[1]), reverse=True)
+    
+    answer.append(alb[0][0])
+    gen = alb[0][2]
+    count = 1
+    for i in alb[1:]:
+        if i[2]==gen:
+            count = count+1
+        else:
+            gen = i[2]
+            count = 1
+        if count>2:
+            pass
+        else:
+            answer.append(i[0])
+    return answer
+    
+    
+    
     
