f55391b94c3de9b9415f484e239d88d9eeade36e
LeeKunHa <kunha98@gmail.com>
2023-10-05 01:52:18 +0900

    [level 2] Title: 전화번호 목록, Time: 80.95 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,8 @@
 def solution(phone_book):
     phone_book = sorted(phone_book)
-    for i,j in zip(phone_book,phone_book[1:]):
-        if i == j[:len(i)]:
-            return False
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+                   return False
     return True
+
+
