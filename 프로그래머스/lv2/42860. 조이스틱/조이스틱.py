f25711c5e7099719defe11730e9f761c307fc1ea
LeeKunHa <kunha98@gmail.com>
2023-10-04 15:58:49 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -1,15 +1,12 @@
-
-
 def solution(name):
     up_down = 0
     left_right = len(name)-1
-    answer=0
-    for i,x in enumerate(name):
-        up_down = up_down+min(ord(x)-ord('A'),ord('Z')-ord(x)+1)
+    for i in range(len(name)):
+        up_down = up_down + min(ord(name[i])-ord('A'), ord('Z')-ord(name[i])+1)
         
         next_ = i+1
-        while next_<len(name) and name[next_]=='A':
-            next_ = next_+1        
-        left_right = min([left_right,(i*2+len(name)-next_),((len(name)-next_)*2+i)])
-    answer = answer+up_down+left_right
+        while next_ < len(name) and name[next_]=='A':
+            next_ = next_+1
+        left_right = min([left_right, 2*i+(len(name)-next_), 2*(len(name)-next_)+i])
+    answer = up_down + left_right
     return answer
