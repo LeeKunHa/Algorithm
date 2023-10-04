7fb3cd116e8dcc049dfb04a9b151a6f2191829f2
LeeKunHa <kunha98@gmail.com>
2023-10-05 01:36:07 +0900

    [level 2] Title: 주식가격, Time: 34.29 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,3 +1,16 @@
+#방법1(리스트,for)
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        time = 0
+        for j in prices[i+1:]:
+            time = time+1
+            if prices[i]>j:
+                break
+        answer.append(time)
+    return answer
+
+# 방법2(deque, while)
 from collections import deque
 def solution(prices):
     queue = deque(prices)
