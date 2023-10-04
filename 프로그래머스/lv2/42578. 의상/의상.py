2488370f75c05f2f80c9abf62d46601c99468a33
LeeKunHa <kunha98@gmail.com>
2023-10-05 02:02:37 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,8 +1,6 @@
 def solution(clothes):
-    dic = dict([])
-    for i in clothes:
-        dic[i[0]] = i[1]
+    dic = {x[0]:x[1] for x in clothes}
     answer = 1
-    for i in list(set(dic.values())):
-        answer = answer * (list(dic.values()).count(i)+1)
+    for i in set(dic.values()):
+        answer = answer*(list(dic.values()).count(i)+1)
     return answer-1
