014856e77fac6b7aa9779e9c22bfb5fc56f2525a
LeeKunHa <kunha98@gmail.com>
2023-10-07 01:27:49 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,9 +1,11 @@
 def solution(N, stages):
     answer = []
     for i in range(1,N+1):
-        try:
-            answer.append([i, len([x for x in stages if x>i])/len([x for x in stages if x>=i])])
-        except:
-            answer.append([i,100])
-    answer = [x[0] for x in sorted(answer,key=lambda x:x[1])]
+        total = stages.count(i)
+        if total==0:
+            fail = 0
+        else:
+            fail = total/len([x for x in stages if x>=i])
+        answer.append([i,fail])
+    answer = [x[0] for x in sorted(answer, key=lambda x:x[1], reverse=True)]
     return answer
